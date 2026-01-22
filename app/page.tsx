2ce3deebb84509f1c6083a351c0916439d6f6418
Jose Ferreira <jpedroferreira95@gmail.com>
2026-01-22 16:03:11 +0000

    feat: add header and tabs to main page
    - install shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import Image from "next/image";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="min-h-screen font-sans bg-black">
+      {/* Header Section */}
+      <div className="relative bg-linear-to-r from-indigo-400 to-cyan-400">
+        <header>
+          <div className="relative p-5 text-center ">
+            <h1 className="text-5xl text-amber-50">Find the Perfect Journey</h1>
+            <p className="text-center text-amber-50">
+              Find the best flights, accommodation, and attractions for your
+              next adventure
+            </p>
+          </div>
+        </header>
+      </div>
+      {/* Tabs Section */}
+      <div className="flex justify-center m-5">
+      <Tabs defaultValue="flights">
+        <TabsList>
+          <TabsTrigger value="flights">✈️ Flights</TabsTrigger>
+          <TabsTrigger value="Hotels">🏢 Hotels</TabsTrigger>
+          <TabsTrigger value="Attractions">🏰 Attrations</TabsTrigger>
+        </TabsList>
+
+      </Tabs>
+      </div>
+    </div>
   );
 }
